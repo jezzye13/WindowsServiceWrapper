ab954bbe3111a125065fbd6f2c51ff819c71b93c
Jessey <jd.jessey@gmail.com>
2022-11-10 11:21:32 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsServiceWrapper")]
+[assembly: AssemblyDescription("Start and kill a specific executable.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WisdomUnit")]
+[assembly: AssemblyProduct("WindowsServiceWrapper")]
+[assembly: AssemblyCopyright("Copyright © WisdomUnit 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4bf5df14-7059-41bd-b69f-5c1717e7a421")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceWrapper
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Process m_process;
+        private readonly string m_executablePath = @"V:\MCServerSoft\mcss.exe";
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            m_process = Process.Start(m_executablePath);
+        }
+
+        protected override void OnStop()
+        {
+            m_process.Kill();
+        }
+    }
+}
